3bde45ba357e152da98c9d9dc6dde4c38c6c28d2
Bhargav Raut <bhargav.r.raut@gmail.com>
2020-12-25 18:06:26 +0530

    checked and working with es-6.3.0, logstash-6.7.0

--- make_cert.sh
+++ make_cert.sh
@@ -2,5 +2,5 @@
 echo "LOGSTASH PATH : ${LOGSTASH_PATH}"
 rm -r $LOGSTASH_PATH/ssl_certificates
 mkdir $LOGSTASH_PATH/ssl_certificates
-$ELASTICSEARCH_PATH/bin/elasticsearch-certutil cert --in instances.yml --out $LOGSTASH_PATH/ssl_certificates/certificates.zip  --keep-ca-key --pem
+$ELASTICSEARCH_PATH/bin/elasticsearch-certutil cert --name host --ip $IP_ADDRESS --out $LOGSTASH_PATH/ssl_certificates/certificates.zip  --keep-ca-key --pem
 unzip $LOGSTASH_PATH/ssl_certificates/certificates.zip -d $LOGSTASH_PATH/ssl_certificates
