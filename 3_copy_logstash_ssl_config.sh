1dc0dcfd8005ac1cfa849cd534d4c1111d3b304b
Bhargav Raut <bhargav.r.raut@gmail.com>
2020-12-30 16:57:06 +0530

    working version till check_cert

--- 3_copy_logstash_ssl_config.sh
+++ 3_copy_logstash_ssl_config.sh
@@ -2,4 +2,9 @@
 LOGSTASH_PATH=${LOGSTASH_PATH:-/etc/logstash/conf.d} 
 echo "using logstash configuration path as "
 echo $LOGSTASH_PATH
-cp ./logstash_ssl.conf $LOGSTASH_PATH
+value=`cat ./logstash_ssl.conf`
+ESCAPED_REPLACE=$(printf '%s\n' "$SSL_CERTIFICATES_PATH" | sed -e 's/[\/&]/\\&/g')
+value=$(echo "$value" | sed "s/\${LOGSTASH_PORT}/$LOGSTASH_PORT/g")
+value=$(echo "$value" | sed "s/\${SSL_CERTIFICATES_PATH}/$ESCAPED_REPLACE/g")
+echo $value
+echo $value > $LOGSTASH_PATH/logstash_ssl.conf
