ac8f402c45faaf1f5e4bdc644025c7f21052de4c
Bhargav Raut <bhargav.r.raut@gmail.com>
2021-01-17 18:52:22 +0530

    added more mappings

--- 3b_create_es_index.sh
+++ 3b_create_es_index.sh
@@ -4,12 +4,16 @@ ES_USER=${ES_USER:-elastic}
 ES_PASSWORD=${ES_PASSWORD:-}
 ES_INDEX=${ES_INDEX:-logstash-logs}
 
+echo $ES_HOST
+echo $ES_USER
+echo $ES_PASSWORD
+echo $ES_INDEX
 ############# UPDATE ELASTICSEARCH INDEX MAPPINGS ###############
 
 echo "creating logstash logs elasticsearch index ---" 
 
 full_host="https://${ES_USER}:${ES_PASSWORD}@${ES_HOST}"
-
+echo "full host ${full_host}"
 echo "deleting index"
 curl -X DELETE "${full_host}/${ES_INDEX}?pretty"
 
@@ -26,6 +30,12 @@ curl -X PUT "${full_host}/${ES_INDEX}/_mapping/doc?pretty" -H 'Content-Type: app
       "type": "integer"
     },
     "tags" : {
+    	"type" : "keyword"
+    },
+    "message": {
+	"type" : "keyword"
+    },
+    "params" : {
     	"type" : "keyword"
     }
   }
